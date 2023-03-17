68d1994a677ee76e6fc7c2000190ea3cb3f60a31
kush Muhamad <kushmohammed98@yahoo.com>
2023-03-17 23:07:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const saturdayFun = (params = "roller-skate") => {
+  return `This Saturday, I want to ${params}!`;
+};
+
+const mondayWork = (activity = "go to the office") => {
+  return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(star = "*") {
+  return function (special = "special") {
+    return `You are ${star}${special}${star}!`;
+  };
+}
+let or = wrapAdjective('||')
+console.log(or)
